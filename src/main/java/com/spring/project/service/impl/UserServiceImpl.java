8043f771ef4340906859868e70d0ca4a11d974d9
Govardhan <jadhav.govardhan93@gmail.com>
2023-01-11 22:22:13 +0530

    user updated

--- src/main/java/com/spring/project/service/impl/UserServiceImpl.java
+++ src/main/java/com/spring/project/service/impl/UserServiceImpl.java
@@ -1,11 +1,13 @@
 package com.spring.project.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.spring.project.model.User;
 import com.spring.project.repository.UserRepository;
 import com.spring.project.service.UserServices;
 
+@Service
 public class UserServiceImpl implements UserServices {
 	@Autowired
 	private UserRepository userRepository;
