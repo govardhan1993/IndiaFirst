42b57bbc8831cea8ba3d4577a5952d5c7207ace0
Govardhan <jadhav.govardhan93@gmail.com>
2023-01-11 20:11:40 +0530

    First Commit

---
+++ src/main/java/com/spring/project/IndiaFirstApplication.java
@@ -0,0 +1,13 @@
+package com.spring.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndiaFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndiaFirstApplication.class, args);
+	}
+
+}
